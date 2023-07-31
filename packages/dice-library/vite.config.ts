f79edb1824794c68d1675a94994284d792c6369e
Lucas Kennedy <lukakfp@gmail.com>
2023-07-31 01:35:32 -0300

    feat: Convert repo to monorepo for playground tests

--- packages/dice-library/vite.config.ts
+++ packages/dice-library/vite.config.ts
@@ -1,7 +1,13 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
